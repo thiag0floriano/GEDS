3dff828ac4a306745f4ecd66a14ef8c2b754500a
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-11-10 10:26:20 -0300

    Limpando o código

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/User');
-
-// const router = express.Router();
-
-// router.post('/register', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const user = await User.create({ username, password });
-//     res.status(201).send({ message: 'User registered successfully' });
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
-// });
-
-// router.post('/login', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const user = await User.findOne({ where: { username } });
-//     if (!user || !(await user.comparePassword(password))) {
-//       return res.status(401).send({ message: 'Invalid username or password' });
-//     }
-//     const token = jwt.sign({ userId: user.id }, 'your_jwt_secret', { expiresIn: '1h' }); // Expira em 1 hora
-//     console.log('Token generated:', token); // Adicione este log
-//     res.send({ token });
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
-// });
-
-// module.exports = router;
-
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
